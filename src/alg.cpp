e38948040c0104e55e47693f65b397bf6d5ed34c
VarvaraSed <varvarasedyshevafgh@gmail.com>
2026-03-18 23:16:08 +0300

    Update_alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -14,10 +14,12 @@ uint64_t collatzMaxValue(uint64_t num) {
         } else {
             currentN = 3 * currentN + 1;
         }
+
         if (currentN > maxValue) {
             maxValue = currentN;
         }
     }
+
     return maxValue;
 }
 
@@ -26,36 +28,40 @@ unsigned int collatzLen(uint64_t num) {
     if (num == 1) return 1;
 
     unsigned int length = 1;
-    uint64_t currentNum = num;
+    uint64_t currentN = num;
 
-    while (currentNum != 1) {
-        if (currentNum % 2 == 0) {
-            currentNum = currentNum / 2;
+    while (currentN != 1) {
+        if (currentN % 2 == 0) {
+            currentN = currentN / 2;
         } else {
-            currentNum = 3 * currentNum + 1;
+            currentN = 3 * currentN + 1;
         }
         length++;
     }
+
     return length;
 }
 
-unsigned int seqCollatz(unsigned int *maxlen, uint64_t lbound, uint64_t rbound) {
+unsigned int seqCollatz(unsigned int *maxlen,
+                        uint64_t lbound,
+                        uint64_t rbound) {
     if (lbound > rbound || lbound < 2) {
         *maxlen = 0;
         return 0;
     }
 
-    unsigned int bestLength = 0;
-    uint64_t bestNumber = lbound;
+    unsigned int topLength = 0;
+    unsigned int bestNumber = lbound;
 
     for (uint64_t num = lbound; num <= rbound; num++) {
         unsigned int currentLength = collatzLen(num);
-
-        if (currentLength > bestLength) {
-            bestLength = currentLength;
+        if (currentLength > topLength) {
+            topLength = currentLength;
             bestNumber = num;
         }
     }
-    *maxlen = bestLength;
+
+    *maxlen = topLength;
+
     return bestNumber;
 }
